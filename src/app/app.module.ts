edbd027c0f9d7aea3a3b598e64d8566842835728
julienschollaert <81239865+julienschollaert@users.noreply.github.com>
2022-09-22 11:34:24 +0200

    commit 22/09/2022 matin
    Création d'un premier composant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PremierComposantComponent } from './premier-composant/premier-composant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PremierComposantComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/premier-composant/premier-composant.component.ts
@@ -0,0 +1,11 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-premier-composant',
+  templateUrl: './premier-composant.component.html',
+  styleUrls: ['./premier-composant.component.scss']
+})
+export class PremierComposantComponent {
+
+
+}
