288c79ac308422a5ee47d3a3e6cefc8a1d1a25bf
julienschollaert <81239865+julienschollaert@users.noreply.github.com>
2022-09-26 15:29:52 +0200

    commit 26 septembre 2022 15H

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import {PremierComposantModel} from "./models/premierModel";
 export class AppComponent implements OnInit {
   firstModel!: PremierComposantModel;
   secondModel!: PremierComposantModel;
+  threeModel!: PremierComposantModel;
 
 
   ngOnInit() {
@@ -17,18 +18,29 @@ export class AppComponent implements OnInit {
       "Honda",
       81,
       new Date(),
-      "https://upload.wikimedia.org/wikipedia/fr/3/36/Red_Bull_Racing_2022.png",
+      "https://www.oracle.com/oce/press/assets/CONT627DB01268AB4125A078BA8CD375ADEF/native/og-social-redbull.gif",
       0,
-      0
+      0,
+      545
     );
     this.secondModel = new PremierComposantModel(
       "Mercedes AMG Petronas Formula One Team",
       "Mercedes",
       124,
       new Date(),
-      "https://upload.wikimedia.org/wikipedia/commons/f/fb/Mercedes_AMG_Petronas_F1_Logo.svg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5I1yaMBjQxnc62DTvuAf-3SQ4PY2fv__0aOKTQFoce0c0CaEwawnTOLWh58fwSBNUyGs&usqp=CAU",
       0,
       0
+    );
+    this.threeModel = new PremierComposantModel(
+      "Scuderia Ferrari",
+      "Ferrari",
+      242,
+      new Date(),
+      "http://www.eldoradojewels.com/img/cms/Brand/Scuderia_Ferrari_Logo.png",
+      0,
+      0,
+      406
     );
   }
 }
--- src/app/models/premierModel.ts
+++ src/app/models/premierModel.ts
@@ -1,13 +1,15 @@
 export class PremierComposantModel {
-  team:string;
-  engine: string;
-  nbVictory: number;
-  dateJours : Date;
-  image : string;
-  comptLike : number;
-  comptDislike : number;
+  team! :string;
+  engine! : string;
+  nbVictory! : number;
+  dateJours! : Date;
+  image! : string;
+  comptLike! : number;
+  comptDislike! : number;
+  seasonPoint? : number;
+// ? = element optionnelle
 
-  constructor(team: string, engine: string, nbVictory: number, dateJours: Date, image: string, comptLike: number, comptDislike: number) {
+  constructor(team: string, engine: string, nbVictory: number, dateJours: Date, image: string, comptLike: number, comptDislike: number ,seasonPoint?: number) {
     this.team = team;
     this.engine = engine;
     this.nbVictory = nbVictory;
@@ -15,5 +17,8 @@ export class PremierComposantModel {
     this.image = image;
     this.comptLike = comptLike;
     this.comptDislike = comptDislike;
+    this.seasonPoint = seasonPoint;
   }
+
+
 }
